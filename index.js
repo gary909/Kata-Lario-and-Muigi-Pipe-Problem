29dfccf69383fcc296a2603b1494e014b504ab6a
gary <gary.white@lodgit.com>
2022-02-16 13:23:53 +0100

    code working

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function pipeFix(numbers){
-
+    let myArr = [];
+    var firstElement  = numbers[0]
+    var lastElement = numbers[numbers.length - 1]
+    for (var i = firstElement; i <= lastElement; i++){
+        myArr.push(i);
+    }
+    return myArr;
 }
 
 console.log(pipeFix([1,2,3,5,6,8,9])); // [1,2,3,4,5,6,7,8,9]
