651aa7d9bafeeb1d4bd67e2877b094debf0feffe
gary <gary.white@lodgit.com>
2022-02-16 13:11:34 +0100

    code

---
+++ index.js
@@ -0,0 +1,7 @@
+function pipeFix(numbers){
+
+}
+
+console.log(pipeFix([1,2,3,5,6,8,9])); // [1,2,3,4,5,6,7,8,9]
+console.log(pipeFix([1,2,3,12])); // [1,2,3,4,5,6,7,8,9,10,11,12]
+console.log(pipeFix([6,9])); // [6,7,8,9]
